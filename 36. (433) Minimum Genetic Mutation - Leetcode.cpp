fca0a32b3e2de739cb2fad7b515cda519251e29c
whorishi <0710rishi@gmail.com>
2023-01-12 22:41:01 +0530

    Adobe Ques 6

--- 36. (433) Minimum Genetic Mutation - Leetcode.cpp
+++ 36. (433) Minimum Genetic Mutation - Leetcode.cpp
@@ -5,7 +5,6 @@ For example, "AACCGGTT" --> "AACCGGTA" is one mutation.
 There is also a gene bank bank that records all the valid gene mutations. A gene must be in bank to make it a valid gene string.
 
 Given the two gene strings startGene and endGene and the gene bank bank, return the minimum number of mutations needed to mutate from startGene to endGene. If there is no such a mutation, return -1.
-
 Note that the starting point is assumed to be valid, so it might not be included in the bank.
 */
 
