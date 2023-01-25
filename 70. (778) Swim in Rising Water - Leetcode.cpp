5ff018ecf7b155f40ba3fc6ff3a93c03619620f4
whorishi <0710rishi@gmail.com>
2023-01-25 18:34:27 +0530

    Google Ques 10

---
+++ 70. (778) Swim in Rising Water - Leetcode.cpp
@@ -0,0 +1,33 @@
+/*
+You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+
+The rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+*/
+
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size(), ans = max(grid[0][0], grid[n-1][n-1]);
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        visited[0][0] = 1;
+        vector<int> dir({-1, 0, 1, 0, -1});
+        pq.push({ans, 0, 0});
+        while (!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
+            ans = max(ans, cur[0]);
+            for (int i = 0; i < 4; ++i) {
+                int r = cur[1] + dir[i], c = cur[2] + dir[i+1];
+                if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0) {
+                    if (r == n-1 && c == n-1) return ans;
+                    pq.push({grid[r][c], r, c});
+                    visited[r][c] = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
